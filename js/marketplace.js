eb63887ba2edc8d1122389cf4f1bd39ec320d062
SAMEER KHAN <sameerk41207@gmail.com>
2026-01-17 14:49:02 +0530

    updated

--- js/marketplace.js
+++ js/marketplace.js
@@ -1,12 +1,25 @@
-// Load Navbar
-fetch("navbar.html")
-  .then(res => res.text())
-  .then(data => { document.getElementById("navbar").innerHTML = data; });
 
-// Load Footer
-fetch("footer.html")
-  .then(res => res.text())
-  .then(data => { document.getElementById("footer").innerHTML = data; });
+  // ----------------------
+  // LOAD NAVBAR
+  // ----------------------
+  fetch("navbar.html")
+    .then(res => res.text())
+    .then(data => {
+      const navbarDiv = document.getElementById("navbar");
+      if(navbarDiv) navbarDiv.innerHTML = data;
+      initNavbarScripts();
+      setActiveNavLink();
+    });
+
+  // ----------------------
+  // LOAD FOOTER
+  // ----------------------
+  fetch("footer.html")
+    .then(res => res.text())
+    .then(data => {
+      const footerDiv = document.getElementById("footer");
+      if(footerDiv) footerDiv.innerHTML = data;
+    });
 
 document.addEventListener("DOMContentLoaded", () => {
   const marketplaceDiv = document.getElementById("marketplaceContainer");
